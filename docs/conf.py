cad3fc28a8b574347a7fd587afcb7db6b47eab1a
Michal Staniaszek <m.staniaszek@gmail.com>
2017-05-11 10:57:07 +0100

    no tex generation

--- docs/conf.py
+++ docs/conf.py
@@ -102,31 +102,31 @@ htmlhelp_basename = 'StrandsDocumentationdoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
+# latex_elements = {
+#     # The paper size ('letterpaper' or 'a4paper').
+#     #
+#     # 'papersize': 'letterpaper',
 
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
+#     # The font size ('10pt', '11pt' or '12pt').
+#     #
+#     # 'pointsize': '10pt',
 
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
+#     # Additional stuff for the LaTeX preamble.
+#     #
+#     # 'preamble': '',
 
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
+#     # Latex figure (float) alignment
+#     #
+#     # 'figure_align': 'htbp',
+# }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'StrandsDocumentation.tex', u'Strands Documentation Documentation',
-     u'Michal Staniaszek', 'manual'),
-]
+# latex_documents = [
+#     (master_doc, 'StrandsDocumentation.tex', u'Strands Documentation Documentation',
+#      u'Michal Staniaszek', 'manual'),
+# ]
 
 
 # -- Options for manual page output ---------------------------------------
@@ -144,11 +144,11 @@ man_pages = [
 # Grouping the document tree into Texinfo files. List of tuples
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'StrandsDocumentation', u'Strands Documentation Documentation',
-     author, 'StrandsDocumentation', 'One line description of project.',
-     'Miscellaneous'),
-]
+# texinfo_documents = [
+#     (master_doc, 'StrandsDocumentation', u'Strands Documentation Documentation',
+#      author, 'StrandsDocumentation', 'One line description of project.',
+#      'Miscellaneous'),
+# ]
 
 
 
